eacecc6cbb3ef2774ba45ca1e2711173c4c60ecc
Eamonn <eamonn@buss.us>
2018-10-07 17:46:40 -0400

    Call script with this

--- src/com/srcdevbin/SourceUtils.groovy
+++ src/com/srcdevbin/SourceUtils.groovy
@@ -6,30 +6,12 @@ class SourceUtils implements Serializable {
 	def url
 	def script
 	
-	SourceUtils(script) { 
+	SourceUtils(script) {
 		this.script = script 
 	}
 	
 	def checkoutSource(Closure cl) {
 		cl()
-		script {
-			script.sh "echo hello - ${url}"
-		}
-	}
-	
-//	def invokeClosure(cl) {
-//		Closure clonedCl = cl.clone()
-//		clonedCl.delegate = this
-//		clonedCl.resolveStrategy = Closure.DELEGATE_FIRST
-//		script.println clonedCl.delegate
-//		clonedCl()
-//		script.println "The URL - ${url}"
-//	}
-	
-	def script(Closure cl) {
-		Closure clonedCl = cl.clone()
-		clonedCl.delegate = script
-		clonedCl.resolveStrategy = Closure.OWNER_FIRST
-		clonedCl()
+		script.sh "echo hello - ${this.url}"
 	}
 }
