30c9098fc6ef88957a0813bdb868262d60f37e73
Eamonn <eamonn@buss.us>
2018-10-07 18:27:54 -0400

    Add resolve strategy

--- src/com/srcdevbin/SourceUtils.groovy
+++ src/com/srcdevbin/SourceUtils.groovy
@@ -10,11 +10,16 @@ class SourceUtils implements Serializable {
 	}
 	
 	def checkoutSource(Closure cl) {
-		def params = [:]
-		cl.delegate = params
-		cl()
-		println params
+		def params = resolveParams(cl)
 		script.println params.url
 		script.sh "echo hello - ${params.url}"
+	}
+	
+	def resolveParams(Closure cl) {
+		def params = [:]
+		cl.resolveStrategy = Closure.DELEGATE_FIRST
+		cl.delegate = params
+		cl()
+		return params
 	}
 }
