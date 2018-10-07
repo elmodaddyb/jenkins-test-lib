dbc3da9bef9a2c524d4da6334a56b5745b72b010
Eamonn <eamonn@buss.us>
2018-10-07 17:39:21 -0400

    immediately invoke closure

--- src/com/srcdevbin/SourceUtils.groovy
+++ src/com/srcdevbin/SourceUtils.groovy
@@ -6,25 +6,25 @@ class SourceUtils implements Serializable {
 	def url
 	def script
 	
-	SourceUtils(script) {
-		this.script = script
+	SourceUtils(script) { 
+		this.script = script 
 	}
 	
 	def checkoutSource(Closure cl) {
-		invokeClosure(cl)
+		cl()
 		script {
 			script.sh "echo hello - ${url}"
 		}
 	}
 	
-	def invokeClosure(cl) {
-		Closure clonedCl = cl.clone()
-		clonedCl.delegate = this
-		clonedCl.resolveStrategy = Closure.DELEGATE_FIRST
-		script.println clonedCl.delegate
-		clonedCl()
-		script.println "The URL - ${url}"
-	}
+//	def invokeClosure(cl) {
+//		Closure clonedCl = cl.clone()
+//		clonedCl.delegate = this
+//		clonedCl.resolveStrategy = Closure.DELEGATE_FIRST
+//		script.println clonedCl.delegate
+//		clonedCl()
+//		script.println "The URL - ${url}"
+//	}
 	
 	def script(Closure cl) {
 		Closure clonedCl = cl.clone()
